b9b124599eebf36f27cc561ba47f5815ee04f811
GNANKADJA Yao Stephane <gnanyaosteph03@gmail.com>
2022-10-16 02:34:10 -0700

    sec ouf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	if (a > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5/n", n, a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
 	else if (a == 0)
 	{
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 0\n", n, a);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
 }
