98613b3b95ecfead9dae5f20777ff9ee144686b6
stephsyg <113688500+stephsyg@users.noreply.github.com>
2022-10-18 21:57:36 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,3 +14,21 @@ int print_last_digit(int n)
 
 	_putchar(ld + '0');
 }
+
+#include <limits.h>
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = print_last_digit(98);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
+}
