fb989609c3b56429e00556ce91d6ea568c244625
GNANKADJA Yao Stephane <gnanyaosteph03@gmail.com>
2022-10-18 10:13:56 -0700

    alpha in lowercase with putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alophabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
