93ad1ac843a6d205cc489a7f93d0c701ed4c64d4
stephsyg <113688500+stephsyg@users.noreply.github.com>
2022-10-18 23:34:44 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,8 +20,8 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 0);
-			_putchar ((prod % 10) + 0);
+				_putchar((prod / 10) + '0');
+			_putchar ((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
