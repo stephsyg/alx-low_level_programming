ef3367750c4855ef45d71e3e330f0aeb0bc3fd05
GNANKADJA Yao Stephane <gnanyaosteph03@gmail.com>
2022-10-28 08:27:01 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,23 +6,17 @@
  * @src: copy from
  * Return: pointer to dest
  */
-char *_strcat(char *dest, char *src)
+char*_stract(char*dest, const char*src)
 {
-	int i;
-	int j;
+	int index = 0, dest_len = 0;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+		int index = 0, dest_len + 0;
+
+		while (dest[index++]
+			dest_len++;
+
+		for (index = 0; src[index]; index++)
+			dest[dest_len++] = src[index];
+
+		return (dest);
 }
