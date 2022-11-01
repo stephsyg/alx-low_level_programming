5ad8b828c97644572cc89a3df84727323f0daeb4
stephsyg <113688500+stephsyg@users.noreply.github.com>
2022-11-01 23:16:41 +0000

    corectionof second task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,21 +9,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+        int index = 0, dest_len = 0;
 
-	i = 0;
-	while (dest[i] != '\0)
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+        while (dest[index++])
+                dest_len++;
+
+        for (index = 0; src[index] && index < n; index++)
+                dest[dest_len++] = src[index];
+
+        return (dest);
 }
