9e4aa24a627485d78f9f9f0442c73e15b9e1ebf5
GNANKADJA Yao Stephane <gnanyaosteph03@gmail.com>
2022-11-01 16:00:35 -0700

    second task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,12 +10,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	 while (dest[index++])
-		  dest_len++;
-
-	 for (index = 0; src[index] && index < n; index++)
-		 dest[dest_len++] = src[index];
-	 return (dest);
+	i = 0;
+	while (dest[i] != '\0)
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
