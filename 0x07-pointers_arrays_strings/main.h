f9e3cebd509f05712179be709d98efd820661db6
stephsyg <113688500+stephsyg@users.noreply.github.com>
2022-11-02 20:47:43 +0000

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
+For main.h file 
+
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -12,5 +13,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif
