4bc5398faa9dbf74ef9c79a27cc5e398e2be27fa
stephsyg <113688500+stephsyg@users.noreply.github.com>
2022-11-02 20:44:42 +0000

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _ putchar(char c)
+int _putchar(char c)
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
