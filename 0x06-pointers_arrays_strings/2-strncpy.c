1df1b033db0fdfbfd7fc568c36a5a5aee5e06cca
GNANKADJA Yao Stephane <gnanyaosteph03@gmail.com>
2022-11-03 04:33:16 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-        int index = 0, src_len = 0;
+	int index = 0, src_len = 0;
 
-        while (src[index++])
-                src_len++;
+	while (src[index++])
+		src_len++;
 
-        for (index = 0; src[index] && index < n; index++)
-                dest[index] = src[index];
+	for (index = 0; src[index] && index < n; index++)
+			dest[index] = src[index];
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
-        for (index = src_len; index < n; index++)
-                dest[index] = '\0';
-
-        return (dest);
+	return (dest);
 }
