09c103279f19614f4426be872101d53ac909dbef
GNANKADJA Yao Stephane <gnanyaosteph03@gmail.com>
2022-11-03 05:59:18 -0700

    tasks 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
-#include "main.h"                                                                                                                    
-                                                                                                                                     
+#include "main.h"
 /**
  * _strcmp - Compares pointers to two strings.
  * @s1: A pointer to the first string to be compared.
@@ -11,11 +10,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-        while (*s1 && *s2 && *s1 == *s2)
-        {
-                s1++;
-                s2++;
-        }
-
-        return (*s1 - *s2);                                                                                                          
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
