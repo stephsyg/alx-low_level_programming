d677d27ccaf329b5475c5e2624046cee1ca9b6ff
stephsyg <113688500+stephsyg@users.noreply.github.com>
2022-11-06 20:02:23 +0000

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n byted changed
  */
-char *_strchr(char *s, char c)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	int r = 0;
+	int i = n;
 
-	for (; s[i] >= '\0'; i++)
+	for (; r < i; r++)
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		dest[r] = src[r];
+		n--;
 	}
-	return (0);
+	return (dest);
 }
